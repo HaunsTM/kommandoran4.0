523a213d211519e7237fbbe840ad23833384dbd7
HaunsTM <hans.baldetorp@gmail.com>
2023-11-12 16:45:00 +0100

    filter out events that already have happened

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -30,9 +30,33 @@
     private readonly calendarStore = useCalendarStore();
 
     todaysEvents(): Array<ICalendarEvent> {
-      return this.calendarStore.getTodaysEvents
-    }
+      const today = new Date();
 
+      const startOfToday = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+      const endOfToday = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1, 0, 0, -1);
+
+      const todaysEvents: Array<ICalendarEvent> = 
+        this.calendarStore.currentCalendars
+          .flatMap(calendar => calendar.events)
+          .filter(event => {
+              //filter out todays events
+              const startsToday = event.start >= startOfToday && event.start < endOfToday;
+              const endsToday = event.end > startOfToday && event.end <= endOfToday;
+              const spansOverWholeDay = event.start < startOfToday && endOfToday < event.end;
+
+              const occursToday = startsToday || endsToday || spansOverWholeDay;
+              return occursToday;
+          })
+          .filter(event => {
+              //filter out events that already have happened
+            const eventHasEnded =  event.end < today;
+            const eventWillEndInFuture = !eventHasEnded;
+            return eventWillEndInFuture;
+          })
+          .sort((a, b) => a.start.getTime() - b.start.getTime());
+
+      return todaysEvents;
+    }
   }
   </script>
   
