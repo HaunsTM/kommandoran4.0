3eb1adcd11469690d5a8b025585f55ec17db6cb4
HaunsTM <hans.baldetorp@gmail.com>
2023-11-22 22:10:00 +0100

    Transport

--- src/components/Transport.vue
+++ src/components/Transport.vue
@@ -1,54 +1,58 @@
 <template>
-  <img :src="require(`@/assets/bus32x32.png`)" />
-  <table>
-    <thead>
-      <tr class="border-bottom">
-        <th></th>
-        <th colspan="2" class="border-left">current</th>
-        <th colspan="2" class="border-left">next</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr class="border-bottom">
-        <th>Lund</th>
-        <template v-if="departureLund?.lines[0]">
-          <td class="border-left">{{departureLund?.lines[0]?.journeyTime}}</td>
-          <td>({{departureLund?.lines[0]?.line}})</td>
-        </template>
-        <template v-else>
-          <td class="border-left">-</td>
-          <td>-</td>
-        </template>
-        <template v-if="departureLund?.lines[1]">
-          <td class="border-left">{{departureLund?.lines[1]?.journeyTime}}</td>
-          <td>({{departureLund?.lines[1]?.line}})</td>
-        </template>
-        <template v-else>
-          <td class="border-left">-</td>
-          <td>-</td>
-        </template>
-      </tr>
-      <tr class="border-bottom">
-        <th>Malmö</th>
-        <template v-if="departureMalmo?.lines[0]">
-          <td class="border-left">{{departureMalmo?.lines[0]?.journeyTime}}</td>
-          <td>({{departureMalmo?.lines[0]?.line}})</td>
-        </template>
-        <template v-else>
-          <td class="border-left">-</td>
-          <td>-</td>
-        </template>
-        <template v-if="departureMalmo?.lines[1]">
-          <td>{{departureMalmo?.lines[1]?.journeyTime}}</td>
-          <td>({{departureMalmo?.lines[1]?.line}})</td>
-        </template>
-        <template v-else>
-          <td class="border-left">-</td>
-          <td>-</td>
-        </template>
-      </tr>
-    </tbody>
-  </table>
+  <div>
+    <h2>
+      Transport
+    </h2>
+    <v-table>
+      <thead>
+        <tr>
+          <th></th>
+          <th colspan="2" class="text-center">current</th>
+          <th colspan="2" class="text-center">next</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>Lund</th>
+          <template v-if="departureLund?.lines[0]">
+            <td class="text-right">{{departureLund?.lines[0]?.journeyTime}}</td>
+            <td class="text-left">({{departureLund?.lines[0]?.line}})</td>
+          </template>
+          <template v-else>
+            <td class="text-right">N/A</td>
+            <td class="text-left">N/A</td>
+          </template>
+          <template v-if="departureLund?.lines[1]">
+            <td class="text-right">{{departureLund?.lines[1]?.journeyTime}}</td>
+            <td class="text-left">({{departureLund?.lines[1]?.line}})</td>
+          </template>
+          <template v-else>
+            <td class="text-right">N/A</td>
+            <td class="text-left">N/A</td>
+          </template>
+        </tr>
+        <tr>
+          <th>Malmö</th>
+          <template v-if="departureMalmo?.lines[0]">
+            <td class="text-right">{{departureMalmo?.lines[0]?.journeyTime}}</td>
+            <td class="text-left">({{departureMalmo?.lines[0]?.line}})</td>
+          </template>
+          <template v-else>
+            <td class="text-right">N/A</td>
+            <td class="text-left">N/A</td>
+          </template>
+          <template v-if="departureMalmo?.lines[1]">
+            <td class="text-right">{{departureMalmo?.lines[1]?.journeyTime}}</td>
+            <td class="text-left">({{departureMalmo?.lines[1]?.line}})</td>
+          </template>
+          <template v-else>
+            <td class="text-right">N/A</td>
+            <td class="text-left">N/A</td>
+          </template>
+        </tr>
+      </tbody>
+    </v-table>
+  </div>
 </template>
 
 <script lang="ts">
@@ -81,21 +85,5 @@ export default class KommandoranFooterTransport extends Vue {
 </script>
 
 <style scope>
-table {
-    table-layout: fixed;
-    font-size: 2rem; /* Adjust this value as needed */
-}
 
-td {
-    text-align: right;
-    padding-left: 10px;
-    padding-right: 10px;
-}
-tr.border-bottom td,
-tr.border-bottom th {
-  border-bottom: 1px solid white;
-}
-.border-left {
-  border-left: 1px solid white;
-}
 </style>
