d07976c77c02c864f56f977993dcffa4cf8a7ca2
HaunsTM <hans.baldetorp@gmail.com>
2023-11-28 12:34:03 +0100

    Updated routes for production

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import HomeAssistantView from '../views/HomeAssistantView.vue'
 import ScreenSaverView from '../views/ScreenSaverView.vue'
 
@@ -24,7 +24,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
